3ce57696e079ada1e4670cc543f4da8c1a7e6b6b
Omkar3843 <deshmukhomkar772@gmail.com>
2023-07-25 14:46:08 +0530

    Singup logic with password encryption

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 const app = express();
 
 const preparAndStartServer = () => {
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`server Started on PORT: ${PORT}`);
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,10 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcrypt');
+
+const { SALT } = require('../config/serverConfig');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -32,6 +36,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+  });
+
+  User.beforeCreate((user) => {
+    const encryptedpassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedpassword;
   });
   return User;
 };
