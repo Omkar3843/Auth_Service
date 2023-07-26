815778323a55c43958631607db4bb5383b910559
Omkar3843 <deshmukhomkar772@gmail.com>
2023-07-26 11:53:56 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const bodyparser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-const UserRepository = require('./repository/user-repository');
+// const UserRepository = require('./repository/user-repository');
+const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -17,8 +18,15 @@ const preparAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`server Started on PORT: ${PORT}`);
-        const repo = new UserRepository();
-        const response = await repo.getById(1);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
+
+        const service = new UserService();
+        // const nweToken = service.cretaeToken({email: 'omkar@123gmail.com', id: 1});
+        // console.log("new token is", nweToken);
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9ta2FyQDEyM2dtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2OTAzNTIzNDUsImV4cCI6MTY5MDM1MjM3NX0.ph8pDhOdATlm9FpY76rIL9E_acYL_Svp_U3C5jJf-C8';
+        const response = service.verifyToken(token);
         console.log(response);
     });
 }
